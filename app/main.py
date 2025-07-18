e2e722c42cf81a0bdc94df35491c40a9b9b222bf
Humphrey Hills <konnectedja@gmail.com>
2025-07-18 11:59:14 -0500

    [app] Initialize basic FastAPI template

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+
+from app.core.config import get_settings
+from app.api.v1 import example
+
+load_dotenv()
+settings = get_settings()
+
+app = FastAPI(title=settings.project_name, debug=settings.debug)
+
+app.include_router(example.router, prefix="/api/v1")
+
+
+@app.get("/")
+def read_root() -> dict[str, str]:
+    return {"message": "Welcome"}

c783b67a545cd2607a1c9873a01745b644472d39
Humphrey Hills <konnectedja@gmail.com>
2025-07-18 13:10:19 -0500

    Revert "Applying previous commit."
    This reverts commit 15334920c60c9539f9d18a0725449d5eb3dac108.

--- app/app.py
+++ app/app.py
@@ -4,13 +4,11 @@ from fastapi import FastAPI
 from app.api.v1 import example
 from app.core.config import get_settings
 
+
 def create_app() -> FastAPI:
     settings = get_settings()
 
-    app = FastAPI(
-        title=settings.project_name,
-        debug=settings.debug
-    )
+    app = FastAPI(title=settings.project_name, debug=settings.debug)
 
     app.include_router(example.router, prefix="/api/v1")
 
